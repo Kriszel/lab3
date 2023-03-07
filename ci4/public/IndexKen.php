f540817a705e71d0238e4261a83b6bc7c1286174
KEN <84188493+Kriszel@users.noreply.github.com>
2023-03-07 14:11:49 +0800

    Update IndexKen.php

--- ci4/public/IndexKen.php
+++ ci4/public/IndexKen.php
@@ -288,7 +288,7 @@ body, html {
           }
         ?>
 
-      <form action="/action_page.php" target="_blank">
+      <form action="dbrecords.php" target="_blank">
         <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
           <div class="w3-half">
             <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
